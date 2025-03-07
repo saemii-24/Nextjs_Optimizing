537cc6b13cc6540ef44b545a7a62204c20d1daeb
saemii-24 <hims2419@gmail.com>
2025-03-07 19:54:58 +0900

    feat: image

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.jaturi.kr',
+				port: '',
+				pathname: '/**',
+				search: '',
+			},
+		],
+	},
+};
